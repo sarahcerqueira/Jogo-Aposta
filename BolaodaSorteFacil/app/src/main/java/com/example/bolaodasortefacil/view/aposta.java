945f0cedfd0aa30006fb6269e3f002366e72b9e7
Sarah Pereira <sarahecomp@gmail.com>
2019-07-25 22:16:31 -0300

    Tabela de entrada de valores da aposta funcionando

---
+++ BolaodaSorteFacil/app/src/main/java/com/example/bolaodasortefacil/view/aposta.java
@@ -0,0 +1,20 @@
+package com.example.bolaodasortefacil.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import com.example.bolaodasortefacil.R;
+
+
+
+public class aposta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aposta2);
+
+    }
+
+
+}
